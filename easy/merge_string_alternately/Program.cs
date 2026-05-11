6c353ccbbdb90fb00736250cb41abd12a589bb98
tkjelds <tkjelds385@gmail.com>
2026-05-11 16:31:19 +0200

    added solution for merge_string

---
+++ easy/merge_string_alternately/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Solution {
+    public string MergeAlternately(string word1, string word2) {
+        char[] w1 = [.. word1];
+        char[] w2 = [.. word2];
+        char[] res = new char[word1.Length + word2.Length];
+
+
+        var l1 = 0;
+        var l2 = 0; 
+
+        while(l1 < w1.Length || l2 < w2.Length)
+        {
+            if(l1 != word1.Length)
+            {
+                res[l1+l2] = w1[l1];
+                l1++;
+            }
+            if(l2 != word2.Length)
+            {
+                res[l1+l2] = w2[l2];
+                l2++;
+            }
+        }
+        return new string(res);
+    }
+    static void Main(string[] args)
+    {
+        Solution sol = new();
+        System.Console.WriteLine($"The resuls is {sol.MergeAlternately("fisk123","fisk").ToString()}");
+    }
+}
